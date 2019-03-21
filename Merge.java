28f12cd2b79dfd0334b5b51d2b6e494fe645c1a9
Brandon Chao <bchao1@stuy.edu>
2019-03-20 21:32:06 -0400

    skeleton for just having arrays as parameters, low and high don't make sense in that context?

--- Merge.java
+++ Merge.java
@@ -2,55 +2,11 @@ public class Merge {
 
   /*sort the array from least to greatest/*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    mergesorth(data,0,data.length-1);
-  }
-  /*
-  public static void mergesorth(int[]data, int low, int high) {
-    if (low >= high) {
-      return;
-    }
-
-    int pivot = (low + (high-low) + 1) / 2;
-    int[] left = new int[(high-low+1)/2];
-    int[] right = new int[(high-low+1)/2];
-
-    left = makeAry(data,low,pivot);
-    right = makeAry(data,pivot+1,high);
-
-    mergesorth(data,low,pivot);
-    mergesorth(data,pivot+1,high);
-
+    mergesorth();
   }
 
-  public static int[] makeAry(int[] data, int low, int high) {
-    int[] a = new int[high-low+1];
-    int z = 0;
-    for (int x = low; x < high; x++) {
-      a[z] = data[x];
-      z++;
-    }
-    return a;
-  }
-  public static int[] merge(int[]d1, int[]d2) {
-    int[] help = new int[d1.length+d2.length];
-    int x = 0;
-    int y = 0;
-    int index = 0;
-    while (x < d1.length && d2.length > y) {
-      if (d1[x] > d2[y]) {
-        help[index] = d1[x];
-      }
-      else {
-        help[index] = d2[y];
-      }
-      index++;
-    }
 
-    return help;
-  }
-  */
-
-  public static void mergesorth(int[]data,int[]temp,int low, int high) {
+  public static void mergesorth(int[] left ,int[] right, int[] data) {
     if (low >= high) {
       return;
     }
