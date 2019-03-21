c5713da457ff86228e0a44b220373a7a4ea5c37c
Brandon Chao <bchao1@stuy.edu>
2019-03-20 21:37:48 -0400

    preliminary create array method for inbetween levels, and spilting within the mergesrot function

--- Merge.java
+++ Merge.java
@@ -2,7 +2,18 @@ public class Merge {
 
   /*sort the array from least to greatest/*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-    mergesorth();
+    if (data.length < 2) {
+      return;
+    }
+    else {
+      int[] left = makeArray(data, 0, data.length /2);
+      int[] right = makeArray(data, data.length/2, data.length-1);
+
+      mergesort(left);
+      mergesort(right);
+      
+    }
+
   }
 
 
@@ -13,8 +24,20 @@ public class Merge {
 
     int pivot = low + (high -low +1) /2;
   }
+
+  public static int[] makeArray(int[] data, int low, int high) {
+    int[] dummy = new int[(high-low)+1];
+    int index = 0;
+    for (int x = low; x < high; x++) {
+      dummy[index] = data[x];
+      index++;
+    }
+    return dummy;
+  }
+
   public static void main(String[] args) {
     int[] a = {34,12,54,234,634,2};
     mergesort(a);
   }
+
 }
