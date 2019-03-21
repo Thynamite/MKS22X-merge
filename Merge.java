58d6e55abb78bb665199db7681c16e582a4878b7
Brandon Chao <bchao1@stuy.edu>
2019-03-20 22:24:19 -0400

    c+p insertion sort

--- Merge.java
+++ Merge.java
@@ -16,6 +16,40 @@ public class Merge {
     }
 
   }
+  public static void insertionSort(int[] data) {
+  int storage = 0;                                                                      //still can't instantiate ints without a value
+  for (int x = 1; x < data.length; x++) {                                               //start at 1, as a size 1 array is already sorted, and should be comparing to that at the start
+    if (data[x] < data[x-1]) {                                                         //checks if the element needs to be placed inside the sorted(greater value is sorted already on the rightmost of the array)
+      storage = data[x];                                                                //stores the value
+      //System.out.println(storage);                                                    //testing
+      int c = x;                                                                        //since this loop looks at the left value to compare, it would need to start at the stored value
+      while (c > 0 && (data[c-1] > storage)) {                                          //this checks if there is a need to shift the value over to the next one, if it's 0 then you can't shift anymore
+        data[c] = data[c-1];                                                            //this shifts the left value to the current position(this value is already saved by storage and in later cases the old in the new position is irrelevant as it is moved out already)
+        c--;                                                                            //increment that was forgotten in one version
+        //System.out.println(toString(data));                                           //testing for terrbile loops
+      }
+      data[c] = storage;                                                                //once the correct position is reached, the stored value is placed there
+      /*  OLD LOOP
+      for(int sorted = x-1; sorted > -1; sorted--) {                                    //counts backwards from the "sorted array"
+        if (storage < data[sorted]) {                                                  //if the element is greater than the stored (need to be sorted), then shift over
+          data[sorted+1] = data[sorted];
+          if (sorted == 0) {                                                           //issue was that it would not be able to set the lowest value all the way at the beginning, it would only duplicate the original value at the start to the next position
+            data[sorted] = storage;
+          }
+          //System.out.println(toString(data));                                         //test
+          //System.out.println("less");                                                 //test
+        }
+        else if (storage >= data[sorted]){ //here probably issue                        //This checked the remaining cases, even though an else would probably suffice
+          data[sorted+1] = storage;                                                     //the duplicate would be overwritten
+          sorted = -1;                                                                  //makes sure loop ends
+          //storage = 0;                                                                //thought this might've actually done something, but just varying amounts of duplicates in failed cases threw me off
+          //System.out.println(toString(data));
+          //System.out.println("more");
+        }
+        */
+      }
+    }
+  }
 
 
   public static void mergesorth(int[] left ,int[] right, int[] data) {
